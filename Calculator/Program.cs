0b0e140a49640f0e6c3ad05e947eb1b2e202313b
Laura Collins <laura.e.collins1@gmail.com>
2020-02-27 20:39:38 -0600

    step one done

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,21 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter some numbers separated by commas...");
+            var userInput = Console.ReadLine().Split(",");
+            int finalValue;
+            string numberList = "";
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                finalValue = int.Parse(userInput[i]);
+                finalValue *= finalValue;
+                numberList += $"{finalValue}, ";
+            }
+
+            Console.WriteLine(numberList.Remove(numberList.Length - 2, 2));
+
+            Console.ReadKey();
         }
     }
 }
